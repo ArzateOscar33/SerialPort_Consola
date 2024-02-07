fa577abffbbdf29c41871e68e94b192e187b6b19
CesatSoftware2 <software2@cesat.mx>
2024-02-07 13:16:26 -0800

    first commit

---
+++ PruebaClaseCOM/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaClaseCOM
+{
+    public class Program
+    {
+        //private string[] ports = SerialPort.GetPortNames();
+        //string COM = "COM4";
+        //ValidacionCOM vCOM = new ValidacionCOM("COM4");
+        static void Main(string[] args)
+        {
+            // Crear una instancia de ValidacionCOM con el nombre del puerto COM
+            ValidacionCOM validacionCOM = new ValidacionCOM("COM5");
+
+            // Suscribirse al evento DataReceived
+            validacionCOM.DataReceived += OnDataReceived;
+
+            // Abrir la conexión
+            validacionCOM.OpenConnection();
+
+            Console.WriteLine("Presiona cualquier tecla para cerrar la conexión.");
+            Console.ReadKey();
+
+            // Cerrar la conexión
+            validacionCOM.CloseConnection();
+        }
+
+        static void OnDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            // Acceder al comando recibido
+            string receivedCommand = e.ReceivedCommand;
+            Console.WriteLine($"Datos recibidos: {receivedCommand}");
+        }
+    }
+}
---
+++ PruebaClaseCOM/Validacion.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO.Ports;
+
+
+public class ValidacionCOM
+{
+    private SerialPort serialPort;
+
+    public event EventHandler<DataReceivedEventArgs> DataReceived;
+
+    public ValidacionCOM(string portName)
+    {
+        // Constructor que recibe el nombre del puerto COM
+        InitializeSerialPort(portName);
+    }
+
+    public void OpenConnection()
+    {
+        try
+        {
+            // Intentar abrir el puerto serie
+            serialPort.Open();
+            Console.WriteLine("Conexión establecida.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error al abrir el puerto serie: {ex.Message}");
+        }
+    }
+
+    public void CloseConnection()
+    {
+        // Cerrar el puerto serie
+        if (serialPort.IsOpen)
+        {
+            serialPort.Close();
+            Console.WriteLine("Conexión cerrada.");
+        }
+    }
+
+    private void InitializeSerialPort(string portName)
+    {
+        // Inicializar el objeto SerialPort con las propiedades necesarias
+        serialPort = new SerialPort();
+        serialPort.PortName = portName;
+        serialPort.BaudRate = 115200;
+        serialPort.DataBits = 8;
+        serialPort.StopBits = StopBits.One;
+        serialPort.Parity = Parity.None;
+
+        // Manejar el evento DataReceived internamente
+        serialPort.DataReceived += SerialPort_DataReceived;
+    }
+
+    private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+    {
+        // Leer el comando recibido
+        string receivedData = serialPort.ReadExisting();
+
+        // Invocar el evento personalizado DataReceived con el comando
+        DataReceived?.Invoke(this, new DataReceivedEventArgs(receivedData));
+    }
+}
+
+public class DataReceivedEventArgs : EventArgs
+{
+    public string ReceivedCommand { get; }
+
+    public DataReceivedEventArgs(string receivedCommand)
+    {
+        ReceivedCommand = receivedCommand;
+    }
+}
